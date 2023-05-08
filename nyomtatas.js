e812bfbbf9db04948589351d41cdb350441fe9ff
vikinga04 <105458498+vikinga04@users.noreply.github.com>
2023-05-08 21:02:56 +0200

    Add files via upload

--- nyomtatas.js
+++ nyomtatas.js
@@ -1,12 +1,16 @@
 function kalkulal() {
     //Űrlapadatok
-    const szelesseg = 297;
-    const magassag = 420;
-    const papir = document.getElementById('papirtipus').value = "32";
+    const szelesseg = document.getElementById('szelesseg').value;
+    const magassag = document.getElementById('magassag').value;
+    const papir = document.getElementById('papirtipus').value;
     //Számítások
     let terulet = Math.round((szelesseg * magassag) / 10000);
     let koltseg = terulet * papir;
     //Megjelenítés
+
+    document.getElementById('terulet').innerHTML = "terulet";
+    document.getElementById('papir').innerHTML = "papir";
+    document.getElementById('koltseg').innerHTML = koltseg;
     document.getElementById('valasz').style.visibility = "visible";
 }
 
